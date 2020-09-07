1e715dc71f4bc83054c81ed02972b7c8063e4bbe
青木 <qingmu.io@qq.com>
2020-09-07 22:13:43 +0800

    init commit

---
+++ cmd/utils.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"bytes"
+	"github.com/spf13/cobra"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func GetFlagValue(cmd *cobra.Command, arg string, defValue string) string {
+	v, err := cmd.Flags().GetString(arg)
+	if err != nil {
+		panic(err.Error())
+	}
+	if v != "" {
+		return v
+	}
+	return defValue
+}
+
+func ExecLocalCmd(c *exec.Cmd) {
+	var stdBuffer bytes.Buffer
+	mw := io.MultiWriter(os.Stdout, &stdBuffer)
+	c.Stdout = mw
+	c.Stderr = mw
+	if err := c.Run(); err != nil {
+		log.Panic(err.Error())
+	}
+	log.Println(stdBuffer.String())
+}
